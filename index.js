738aa45bcff62c7e68b4c78114816af62bad316d
Franz Lagler <franz.lagler@outlook.com>
2021-09-09 18:14:42 +0200

    Add next tasks

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// Header Content Type
+// Fehlermeldung
+
 const http = require('node:http');
 const fs = require('node:fs');
 const path = require('node:path');
@@ -5,7 +8,7 @@ const path = require('node:path');
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  let fileName = '.' + req.url;
+  let fileName = './public' + req.url;
   console.log(fileName);
   const fileType = path.extname(fileName);
   console.log(fileType);
@@ -13,9 +16,9 @@ const server = http.createServer((req, res) => {
   console.log(path.extname(''));
 
   if (fileName === './favicon.ico') {
-    fileName = './index.html';
+    fileName = './public/index.html';
   } else if (fileName === './') {
-    fileName = './index.html';
+    fileName = './public/index.html';
   }
   fs.access(fileName, (error) => {
     if (error) {
