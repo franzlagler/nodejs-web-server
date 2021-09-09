75cf7eaef074b68206ddb1c6ca079c1b1ac0a503
Franz Lagler <franz.lagler@outlook.com>
2021-09-09 18:07:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 const http = require('node:http');
 const fs = require('node:fs');
+const path = require('node:path');
 
 const port = process.env.PORT || 3000;
 
-fs.readFile('./text.txt', (err, text) => {
-  if (err) {
-    throw err;
+const server = http.createServer((req, res) => {
+  let fileName = '.' + req.url;
+  console.log(fileName);
+  const fileType = path.extname(fileName);
+  console.log(fileType);
+
+  console.log(path.extname(''));
+
+  if (fileName === './favicon.ico') {
+    fileName = './index.html';
+  } else if (fileName === './') {
+    fileName = './index.html';
   }
-
-  const server = http.createServer((req, res) => {
-    res.write(text);
-    res.end();
+  fs.access(fileName, (error) => {
+    if (error) {
+      console.log('Error');
+      res.end();
+    } else {
+      fs.readFile(fileName, (err, file) => {
+        if (err) {
+          console.log(err);
+        }
+        res.write(file);
+        res.end();
+      });
+    }
   });
-
-  server.listen(port, () => {
-    console.log(`Server running at port ${port}`);
-  });
+});
+server.listen(port, () => {
+  console.log(`Server running at port ${port}`);
 });
